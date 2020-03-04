3247a2c5db7372f71e43ee3cc658b72c5c7ab4c9
unknown <wahyuchaosingan@gmail.com>
2020-03-04 18:31:02 +0800

    Video ke 2

---
+++ index.js
@@ -0,0 +1,98 @@
+const electron = require("electron");
+
+const {
+    app,
+    BrowserWindow,
+    Menu,
+    ipcMain
+} = electron;
+
+let todayWindow;
+let createWindow;
+let listWindow;
+
+
+app.on("ready", () => {
+    todayWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        title: "Tutorial Electron"
+    });
+
+    todayWindow.loadURL(`file://${__dirname}/today.html`);
+    todayWindow.on("closed", () => {
+
+        app.quit()
+        todayWindow = null;
+    });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+const listWindowCreator = () => {
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        widht: 600,
+        height: 400,
+        title: "All Appointments"
+    });
+
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("closed", () => (listWindow = null));
+};
+
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        widht: 600,
+        height: 400,
+        title: "Create Appointments"
+    });
+
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+    createWindow.on("closed", () => (createWindow = null));
+};
+
+const menuTemplate = [{
+        label: "file",
+        submenu: [{
+                label: "New Appointments",
+
+                click() {
+                    createWindowCreator();
+                }
+            },
+            {
+                label: "All Appointments",
+                click() {
+                    listWindowCreator();
+                }
+            },
+            {
+                label: "Quit",
+                accelerator: process.platfrom === "darwin" ? "Command+Q" : "CTRL + Q",
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    },
+
+    {
+        label: "View",
+        submenu: [{
+            role: "reload"
+        }, {
+            role: "toggledevtools"
+        }]
+    }
+]
